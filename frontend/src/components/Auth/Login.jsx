6f11bf751c176d4fdb9a6474bbc6a0d096485340
Shahbaz <shahbazathwal2107@gmail.com>
2024-03-12 16:59:16 -0300

    Hosting changes

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/login",
+        "https://light-garment-dog.cyclic.app/api/v1/user/login",
         { email, password, role },
         {
           headers: {
