cae11da71515b44f1df338234e7c091507892f01
Shahbaz <shahbazathwal2107@gmail.com>
2024-03-15 00:44:49 -0300

    *

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL,"*"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
